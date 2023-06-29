6c5b2e284e195eaf039643b4c483dd1f3bdb9c53
DynnoOttu <dynnoottu968@gmail.com>
2023-06-29 14:08:45 +0800

    useState

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button/Button";
 
@@ -7,16 +7,30 @@ const products = [
     {
         id: 1,
         name: "New shoes",
-        price: "278.000",
+        price: 278000,
         images: "/images/shoes-1.jpg",
         descreption: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi cupiditate temporibus ipsum doloribus iure cum inventore magni architecto voluptas doloremque.`
     },
     {
         id: 2,
         name: "Old shoes",
-        price: "100.000",
+        price: 1000000,
         images: "/images/shoes-1.jpg",
         descreption: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.`
+    },
+    {
+        id: 3,
+        name: "Nike",
+        price: 1000000,
+        images: "/images/shoes-1.jpg",
+        descreption: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.`
+    },
+    {
+        id: 4,
+        name: "Adidas",
+        price: 5000000,
+        images: "/images/shoes-1.jpg",
+        descreption: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.`
     }
 ]
 
@@ -24,10 +38,27 @@ const email = localStorage.getItem('email')
 
 const ProductsPage = () => {
 
+    const [cart, setCart] = useState([
+        {
+            id : 1,
+            qty: 1
+        }
+    ])
+
     const handleLogout = () => {
         localStorage.removeItem('email')
         localStorage.removeItem('password')
         window.location.href="/login"
+    }
+
+    const handleAddToCart = (id) => {
+       if(cart.find(item => item.id === id)) {
+        setCart(
+            cart.map(item => item.id === id ? {...item, qty : item.qty + 1} : item)
+        )
+       } else {
+        setCart([...cart, {id, qty: 1}])
+       }
     }
 
     return (
@@ -35,16 +66,43 @@ const ProductsPage = () => {
         <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">{email} 
         <Button classname="ml-5 bg-black" onClick={handleLogout}> Logout</Button></div>
         <div className="flex justify-center py-5">
-            {products.map((products) => (
-                // eslint-disable-next-line react/jsx-key
-                <CardProduct key={products.id}>
-                    <CardProduct.Header image={products.images}/>
-                    <CardProduct.Body name={products.name}>
-                        {products.descreption}
-                    </CardProduct.Body>
-                    <CardProduct.Footer price={products.price}/>
-                </CardProduct>
-            ))}
+            <div className="w-4/6 flex flex-wrap">
+                {products.map((product) => (
+                    <CardProduct key={product.id}>
+                        <CardProduct.Header image={product.images}/>
+                        <CardProduct.Body name={product.name}>
+                            {product.descreption}
+                        </CardProduct.Body>
+                        <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart}/>
+                    </CardProduct>
+                ))}
+            </div>
+            <div className="w-3/6">
+                <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
+                <table className="text-left table-auto border-separate border-spacing-x-5">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cart.map((item) => {
+                            const product = products.find((product) => product.id === item.id )
+                            return (
+                                <tr key={item.id}>
+                                    <td>{product.name}</td>    
+                                    <td>{product.price.toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</td>    
+                                    <td>{item.qty}</td>    
+                                    <td>Rp. {(item.qty * product.price).toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</td>    
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
         </Fragment>
     )
--- src/components/Elements/Button/Button.jsx
+++ src/components/Elements/Button/Button.jsx
@@ -2,9 +2,9 @@ const Button = (props) => {
     // eslint-disable-next-line react/prop-types
     const {children = "data not found", classname ="bg-yellow-500", onClick = () => {}, type="button"} = props;
     return(
-      <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type={type} onClick={() =>  onClick() }>
+      <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type={type} onClick={onClick}>
       {children}
-    </button>
+    </button> 
     )
   }
 
